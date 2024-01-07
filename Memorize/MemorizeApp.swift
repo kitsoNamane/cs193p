4d56927123068e1aa0fe13cc2e0655bb4df19b14
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2024-01-07 15:13:36 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kitso Namane on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
